a46f53fa7423da9206afe92cfe6f0d5ae292945b
Stuart Clennett <stuartclennett@gmail.com>
2018-12-19 10:16:25 +0000

    Added sample batch file script

---
+++ Win32/Release/csvExport.bat
@@ -0,0 +1,6 @@
+csvtoolscmd -tablename=VIEW_IBID_KPI -filename=D:\VIEW_IBID_KPI.csv
+csvtoolscmd -tablename=VIEW_DEP_KPI -filename=D:\VIEW_DEP_KPI.csv
+csvtoolscmd -tablename=VIEW_DEPTX_KPI -filename=D:\VIEW_DEPTX_KPI.csv
+csvtoolscmd -tablename=VIEW_IBID_FDS -filename=D:\VIEW_IBID_FDS.csv
+csvtoolscmd -tablename=VIEW_DEP_FDS -filename=D:\VIEW_DEP_FDS.csv
+csvtoolscmd -tablename=VIEW_DEPTX_FDS -filename=D:\VIEW_DEPTX_FDS.csv
